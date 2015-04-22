a648bc2f5a857c1035ed3ee12f3569648052f661
Aanchal Arora <aancaror@in.ibm.com>
2015-04-22 14:38:23 +0530

    Initial Commit

---
+++ BillPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillPay
+//
+//  Created by Aanchal Arora on 22/04/15.
+//  Copyright (c) 2015 Aanchal Arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillPay
+//
+//  Created by Aanchal Arora on 22/04/15.
+//  Copyright (c) 2015 Aanchal Arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
